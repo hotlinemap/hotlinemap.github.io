db21717ecf4daa61cfef24bc5796f3aff62b0e37
Jonathan Nicholls <jonathanrnicholls@gmail.com>
2017-04-29 11:43:19 +0800

    Added important links files
    I've uploaded the files which add the subpage Important Links, which contains important links. I've also added some JavaScript.

---
+++ script.js
@@ -0,0 +1,3 @@
+function suicideWebsite() {
+	window.alert("Please remember. You should never, no matter what, take you life. It is something that you will regret. Your life can always get better.");
+}
